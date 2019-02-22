5f41cce5e3ba6f0b029ac0427f944cefa99b0ad8
CookieMonsta89 <jlstan06@hotmail.com>
2019-02-22 16:11:50 -0500

    updated opacity div

--- src/components/Individual-Single-Components/MovieInfo/styled-components.js
+++ src/components/Individual-Single-Components/MovieInfo/styled-components.js
@@ -18,7 +18,6 @@ export const MovieInfoDiv = styled.div`
 
 export const OpacityDiv = styled.div`
     width:80%;
-    height:auto;
     background-color:#0e0d0c;
     opacity:0.75;
     color:#f4f4f3
@@ -26,10 +25,18 @@ export const OpacityDiv = styled.div`
     justify-content:center;
     padding:0 20px;
 
+    @media(max-width:500px) {
+        font-size:1rem;
+    }
+
 `;
 
 export const InnerOpacityDiv = styled.div`
     h1 {
         font-size:2rem
+
+        @media(max-width:500px) {
+            font-size:1.3rem;
+        }
     }
 `;
