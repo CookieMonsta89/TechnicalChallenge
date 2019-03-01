6802fa717e6cd8b7a583875f0fd7139216a93625
CookieMonsta89 <jlstan06@hotmail.com>
2019-03-01 16:46:57 -0500

    added google analytics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import HomeView from '../src/components/Home-view/HomeView'
 import Header from '../src/components/Individual-Single-Components/Header/Header'
 import { Route, Switch } from 'react-router-dom'
 import MovieDetails from './components/Single-View/MovieDetails';
+import ReactGA from 'react-ga';
 
 const App = () => {
+
+  function initializeReactGA() {
+    ReactGA.initialize('UA-135427594-1');
+    ReactGA.pageview('/');
+}
+
+
   return (
     <AppDiv>
         <Header />
